68b16870a20073aef1936254b4371b8106acaeed
Cesar Emmanuel <cesaremmmanul@gmail.com>
2024-03-28 09:55:44 -0300

    feat: create new section how-it-works

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/react'
 import Features from './components/features'
 import Hero from './components/hero'
 import Highlights from './components/highlights'
+import HowItWorks from './components/how-it-works'
 import Model from './components/model'
 import Navbar from './components/navbar'
 
@@ -14,6 +15,7 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   )
 }
